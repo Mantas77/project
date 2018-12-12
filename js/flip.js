deb5cec24025825c9ce557b070f3315e5d935b90
Mantas77 <45818374+Mantas77@users.noreply.github.com>
2018-12-12 11:13:43 +0000

    Add files via upload

---
+++ js/flip.js
@@ -0,0 +1,9 @@
+function flip() {
+    $('.cards').toggleClass('flipped');
+}
+function flip1() {
+    $('.card1').toggleClass('flipped');
+}
+function flip2() {
+    $('.card2').toggleClass('flipped');
+}
